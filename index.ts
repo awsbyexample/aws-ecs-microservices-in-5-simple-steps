e11e326e553170cdb4b0b76d55ae8e2e1db2dc41
aGuyNamedJonas <hi@aGuyNamedJonas.com>
2024-08-15 17:16:17 +0200

    Finalize code samples

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // To run one of the examples, simply (un)comment the appropriate import / export pair and run `pulumi up`
 
-// import './01-simple-microservice';
-// export * from './01-simple-microservice';
+import './01-simple-microservice';
+export * from './01-simple-microservice';
 
 // import './02-https-only';
 // export * from './02-https-only';
@@ -12,5 +12,5 @@
 // import './04-multiple-services';
 // export * from './04-multiple-services';
 
-import './05-autoscaling';
-export * from './05-autoscaling';
+// import './05-autoscaling';
+// export * from './05-autoscaling';
